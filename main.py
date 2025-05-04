b373c0e14b58168f8104a9527d79285c59ea419d
ScottWPetty <scottwilsonpetty@gmail.com>
2025-05-04 02:25:28 -0500

    added comments

--- main.py
+++ main.py
@@ -4,13 +4,21 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # game loop
     while True:
+
+        # check if user closed the window.
+        # if they do, exit the game loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+        
+        # fill screen with black color
         screen.fill("black")
-        pygame.display.flip()
+
+        # refresh the screen; (always do this last)
+        pygame.display.flip() 
 
     print(f"Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
